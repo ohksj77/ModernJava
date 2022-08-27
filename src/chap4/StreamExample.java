26b280d4a3480f0eee44b03a7f1c30061aa63c63
ohksj77 <ohksj77@gmail.com>
2022-08-27 23:24:45 +0900

    챕터 4 완독

--- src/chap4/StreamExample.java
+++ src/chap4/StreamExample.java
@@ -3,6 +3,7 @@ package chap4;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static chap4.Dish.menu;
 import static java.util.Comparator.comparing;
@@ -19,5 +20,22 @@ public class StreamExample {
                 .map(Dish::getName).collect(toList());
 
         Map<Dish.Type, List<Dish>> dishesByType = menu.stream().collect(groupingBy(Dish::getType));
+
+        List<String> threeHighCaloricDishNames = menu.stream().filter(dish -> dish.getCalories() > 300).map(Dish::getName)
+                .limit(3).collect(toList());
+
+        List<String> title = Arrays.asList("1", "2", "3");
+        title.stream().forEach(System.out::println);
+
+        List<String> names = menu.stream().filter(dish -> {
+            System.out.println("dish.getCalories() = " + dish.getCalories());
+            return dish.getCalories() > 300;
+        }).map(dish -> {
+            System.out.println("dish.getName() = " + dish.getName());
+            return dish.getName();
+        }).limit(3).collect(toList());
+        System.out.println("names = " + names);
+
+        menu.stream().forEach(System.out::println);
     }
 }
