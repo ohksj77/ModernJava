3ebe9b12ebc280c821cea3711797da90719e9191
ohksj77 <ohksj77@gmail.com>
2022-08-30 22:22:26 +0900

    챕터 5.2 까지

---
+++ src/chap5/StreamUsages.java
@@ -0,0 +1,38 @@
+package chap5;
+
+import chap4.Dish;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static chap4.Dish.menu;
+import static java.util.stream.Collectors.toList;
+
+public class StreamUsages {
+    public static void main(String[] args) {
+
+        List<Dish> vegetarianMenu = menu.stream().filter(Dish::isVegetarian).collect(toList());
+
+        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+
+        numbers.stream().filter(i -> i % 2 == 0).distinct().forEach(System.out::println);
+
+        List<Dish> specialMenu = Arrays.asList(
+                new Dish("seasonal fruit", true, 120, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER)
+
+        );
+
+        List<Dish> slicedMenu1 = specialMenu.stream().takeWhile(dish -> dish.getCalories() < 320).collect(toList());
+
+        List<Dish> slicedMenu2 = specialMenu.stream().dropWhile(dish -> dish.getCalories() < 320).collect(toList());
+
+        List<Dish> dishes1 = specialMenu.stream().filter(dish -> dish.getCalories() > 300).limit(3).collect(toList());
+
+        List<Dish> dishes2 = specialMenu.stream().filter(d -> d.getCalories() > 300).skip(2).collect(toList());
+
+    }
+}
